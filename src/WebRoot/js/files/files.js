b9b0dd162d3bbf603acd421fdfc2a76a11b19608
chenchao5116 <chenchao5116@gmail.com>
2011-06-28 13:38:20 +0000

    删除文件时提示确认信息

--- src/WebRoot/js/files/files.js
+++ src/WebRoot/js/files/files.js
@@ -42,10 +42,14 @@ function openPage(currentPage){
 }
 //删除单条记录
 function deleteOne(deleteParam){
-	var parentId = document.getElementById("parentId").value;		//获取档案目录的上一级目录Id
-	var form = document.getElementById("queryForm1");
-	form.action = "fileAction!deleteFile.action?deleteParams="+deleteParam+"&parentId="+parentId;
-	form.submit();
+	if(confirm('确认需要删除选定的记录?')){
+		var parentId = document.getElementById("parentId").value;		//获取档案目录的上一级目录Id
+		var form = document.getElementById("queryForm1");
+		form.action = "fileAction!deleteFile.action?deleteParams="+deleteParam+"&parentId="+parentId;
+		form.submit();
+	}else{
+		return;
+	}
 }
 //删除多条记录
 function deleteMore(){
@@ -61,9 +65,13 @@ function deleteMore(){
 		}
 	}
 	if(selectFlag){
-		var form = document.getElementById("queryForm1");
-		form.action = "fileAction!deleteFile.action?parentId="+parentId+param;
-		form.submit();
+		if(confirm('确认需要删除选定的记录?')){
+			var form = document.getElementById("queryForm1");
+			form.action = "fileAction!deleteFile.action?parentId="+parentId+param;
+			form.submit();
+		}else{
+			return;
+		}
 	}else{
 		alert('请选择要删除的记录!');
 	}
