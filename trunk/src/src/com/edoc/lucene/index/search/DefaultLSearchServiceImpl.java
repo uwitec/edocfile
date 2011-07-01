369b4626aa9cabaf022019220d75c9fb16c67323
chenchao5116@gmail.com <chenchao5116@gmail.com@77677335-59f9-efb8-26f6-79e81553ad23>
2011-07-01 05:54:32 +0000

    检索时增加过滤条件

--- trunk/src/src/com/edoc/lucene/index/search/DefaultLSearchServiceImpl.java
+++ trunk/src/src/com/edoc/lucene/index/search/DefaultLSearchServiceImpl.java
@@ -1,23 +1,33 @@
 package com.edoc.lucene.index.search;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Component;
 
 import com.edoc.dbsupport.PageValueObject;
 import com.edoc.lucene.index.EdocDocument;
+import com.edoc.service.files.RecycleService;
 import com.edoc.utils.StringUtils;
 @Component("defaultLSearchService")
 public class DefaultLSearchServiceImpl implements LSearchService {
+	@Resource(name="recycleService")
+	private RecycleService recycleService = null;
+	
 
 	public PageValueObject<EdocDocument> keyWordSearch(String keyWord,int currentPage,int pageSize) {
 		PageValueObject<EdocDocument> rs = null;
 		if(StringUtils.isValid(keyWord)){
 			rs = new PageValueObject<EdocDocument>(currentPage,pageSize);
 			SearchManager searchManager = new SearchManager();
-			//获取索引目录
+			
+			//查找回收站中的文档
+			String[] recycleFileIds = recycleService.getRecycleFileIds();
+			
+			//分页查询
 			if(rs.getFirstResult()==0){
-				rs.setResult(searchManager.keyWordSearch(keyWord,rs.getFirstResult(), rs.getPageSize()));
+				rs.setResult(searchManager.keyWordSearch(keyWord,recycleFileIds,rs.getFirstResult(), rs.getPageSize()));
 			}else{
-				rs.setResult(searchManager.keyWordSearch(keyWord,rs.getFirstResult(), rs.getPageSize()));
+				rs.setResult(searchManager.keyWordSearch(keyWord,recycleFileIds,rs.getFirstResult(), rs.getPageSize()));
 			}
 			
 			rs.setTotalRows(searchManager.getRecordCount());
--- trunk/src/src/com/edoc/lucene/index/search/SearchManager.java
+++ trunk/src/src/com/edoc/lucene/index/search/SearchManager.java
@@ -1,22 +1,25 @@
 package com.edoc.lucene.index.search;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
+import org.apache.lucene.index.CorruptIndexException;
 import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.Term;
 import org.apache.lucene.queryParser.ParseException;
 import org.apache.lucene.queryParser.QueryParser;
+import org.apache.lucene.search.BooleanClause;
+import org.apache.lucene.search.BooleanQuery;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.search.TopScoreDocCollector;
 import org.apache.lucene.search.highlight.Highlighter;
 import org.apache.lucene.search.highlight.InvalidTokenOffsetsException;
@@ -75,69 +78,121 @@ public class SearchManager {
 	 * @param keyWord	查询关键字
 	 * @return
 	 */
-	public List<EdocDocument> keyWordSearch(String keyWord,int currentPage,int pageSize){
+	public List<EdocDocument> keyWordSearch(String keyWord,String[] excludeFileIds, int currentPage,int pageSize){
 		List<EdocDocument> rs = null;
 		try {
 			//从本地硬盘上加装索引文件信息
 			File indexDir = new File(ConfigResource.getConfig(ConfigResource.EDOCINDEXFILE));
-			indexReader = IndexReader.open(FSDirectory.open(indexDir));		//从硬盘中读取索引文件
+			indexReader = IndexReader.open(FSDirectory.open(indexDir));
 			searcher = new IndexSearcher(indexReader);
 			
-			Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_30);
-			QueryParser parser = new QueryParser(Version.LUCENE_30, FieldName.FIELD_CONTENT, analyzer);
-			Query query = parser.parse(keyWord);
-			TopScoreDocCollector collector = TopScoreDocCollector.create(TOP_NUM , false);		//查询前TOP_NUM条符合条件的记录
+			//创建查询query
+			Query query = createQuery(keyWord,excludeFileIds);						
+			
+			//分页查询设置
+			TopScoreDocCollector collector = TopScoreDocCollector.create(TOP_NUM , false);
 			searcher.search(query, collector);
 			setRecordCount(collector.getTotalHits());
 			ScoreDoc[] hits = collector.topDocs(currentPage,pageSize).scoreDocs;
 			
-			Highlighter highlighter = null; 
-			SimpleHTMLFormatter simpleHTMLFormatter = new SimpleHTMLFormatter("<font color=\"red\">", "</font>"); 
-		    highlighter = new Highlighter(simpleHTMLFormatter, new QueryScorer(query)); 
-		    highlighter.setTextFragmenter(new SimpleFragmenter(200)); 
-			if(hits!=null){
-				rs = new ArrayList<EdocDocument>(hits.length);
-				EdocDocument edoc = null;
-				Document doc = null;
-				for(ScoreDoc sdoc:hits){
-					doc = searcher.doc(sdoc.doc);//new method is.doc()
-					TokenStream tokenStream = analyzer.tokenStream(EdocDocument.FIELD_CONTENT,new StringReader(doc.get(EdocDocument.FIELD_CONTENT)));
-					edoc = new EdocDocument();
-					edoc.setContents(highlighter.getBestFragment(tokenStream, doc.get(FieldName.FIELD_CONTENT)));
-					if(doc.getField(EdocDocument.FIELD_CREATETIME)!=null){
-						edoc.setCreateTime(doc.getField(EdocDocument.FIELD_CREATETIME).stringValue());
-					}
-					if(doc.getField(EdocDocument.FIELD_CREATORID)!=null){
-						edoc.setCreatorId(doc.getField(EdocDocument.FIELD_CREATORID).stringValue());
-					}
-					if(doc.getField(EdocDocument.FIELD_CREATORNAME)!=null){
-						edoc.setCreatorName(doc.getField(EdocDocument.FIELD_CREATORNAME).stringValue());
-					}
-					if(doc.getField(EdocDocument.FIELD_FILENAME)!=null){
-						edoc.setFileName(doc.getField(EdocDocument.FIELD_FILENAME).stringValue());
-					}
-					if(doc.getField(EdocDocument.FIELD_FILESIZE)!=null){
-						edoc.setFileSize(doc.getField(EdocDocument.FIELD_FILESIZE).stringValue());
-					}
-					if(doc.getField(EdocDocument.FIELD_SOURCEFILEID)!=null){
-						edoc.setSourceFileId(doc.getField(EdocDocument.FIELD_SOURCEFILEID).stringValue());
-					}
-					if(doc.getField(EdocDocument.FIELD_VERSIONNUM)!=null){
-						edoc.setVersionNum(doc.getField(EdocDocument.FIELD_VERSIONNUM).stringValue());
-					}
-					if(doc.getField(EdocDocument.FIELD_SOURCEFILENAME)!=null){
-						edoc.setSourceFileName(doc.getField(EdocDocument.FIELD_SOURCEFILENAME).stringValue());
-					}
-					
-					rs.add(edoc);
-				}
-			}
+			//高亮设置
+			Highlighter highlighter = makeHighlighterFormatter(query); 
+			
+			//解析并返回查询结果集
+		    return parserQueryResults(hits,highlighter);
 		} catch (Exception e) {
-//			e.printStackTrace();
+			e.printStackTrace();
 			System.out.println(e.getMessage());
 		}
 		return rs;
 	}
+
+	/**
+	 * 高亮显示设置
+	 * @param query		查询query
+	 * @return
+	 */
+	private Highlighter makeHighlighterFormatter(Query query) {
+		Highlighter highlighter = null;
+		SimpleHTMLFormatter simpleHTMLFormatter = new SimpleHTMLFormatter("<font color=\"red\">", "</font>"); 
+	    highlighter = new Highlighter(simpleHTMLFormatter, new QueryScorer(query)); 
+	    highlighter.setTextFragmenter(new SimpleFragmenter(200)); 
+		return highlighter;
+	}
+
+	/**
+	 * 创建查询Query
+	 * @param keyWord			查询关键词
+	 * @param excludeFileIds	过滤文档id
+	 * @return
+	 * @throws ParseException
+	 */
+	private Query createQuery(String keyWord, String[] excludeFileIds) throws ParseException {
+		//Query query 
+		QueryParser parser = new QueryParser(Version.LUCENE_30, FieldName.FIELD_CONTENT, new StandardAnalyzer(Version.LUCENE_30));
+		Query query = parser.parse(keyWord);
+		BooleanQuery bQuery = new BooleanQuery() ;
+		bQuery.add(query, BooleanClause.Occur.MUST);
+		//过滤不可见的id对应的文档
+		if(excludeFileIds!=null && excludeFileIds.length>0){
+			for(String fileId:excludeFileIds){
+				bQuery.add( new TermQuery(new Term(EdocDocument.FIELD_SOURCEFILEID,fileId)), BooleanClause.Occur.MUST_NOT );
+			}
+		}
+		return bQuery;
+	}
+	
+	/**
+	 * 解析查询出来的结果
+	 * @param  hits								查询结果
+	 * @param  highlighter						高亮显示设置
+	 * @return
+	 * @throws IOException 
+	 * @throws CorruptIndexException 
+	 * @throws InvalidTokenOffsetsException 
+	 */
+	public List<EdocDocument> parserQueryResults(ScoreDoc[] hits,Highlighter highlighter) throws CorruptIndexException, IOException, InvalidTokenOffsetsException{
+		List<EdocDocument> rs = null;
+		if(hits!=null){
+			rs = new ArrayList<EdocDocument>(hits.length);
+			EdocDocument edoc = null;
+			Document doc = null;
+			for(ScoreDoc sdoc:hits){
+				doc = searcher.doc(sdoc.doc);//new method is.doc()
+				TokenStream tokenStream = new StandardAnalyzer(Version.LUCENE_30).tokenStream(EdocDocument.FIELD_CONTENT,new StringReader(doc.get(EdocDocument.FIELD_CONTENT)));
+				edoc = new EdocDocument();
+				edoc.setContents(highlighter.getBestFragment(tokenStream, doc.get(FieldName.FIELD_CONTENT)));
+				if(doc.getField(EdocDocument.FIELD_CREATETIME)!=null){
+					edoc.setCreateTime(doc.getField(EdocDocument.FIELD_CREATETIME).stringValue());
+				}
+				if(doc.getField(EdocDocument.FIELD_CREATORID)!=null){
+					edoc.setCreatorId(doc.getField(EdocDocument.FIELD_CREATORID).stringValue());
+				}
+				if(doc.getField(EdocDocument.FIELD_CREATORNAME)!=null){
+					edoc.setCreatorName(doc.getField(EdocDocument.FIELD_CREATORNAME).stringValue());
+				}
+				if(doc.getField(EdocDocument.FIELD_FILENAME)!=null){
+					edoc.setFileName(doc.getField(EdocDocument.FIELD_FILENAME).stringValue());
+				}
+				if(doc.getField(EdocDocument.FIELD_FILESIZE)!=null){
+					edoc.setFileSize(doc.getField(EdocDocument.FIELD_FILESIZE).stringValue());
+				}
+				if(doc.getField(EdocDocument.FIELD_SOURCEFILEID)!=null){
+					edoc.setSourceFileId(doc.getField(EdocDocument.FIELD_SOURCEFILEID).stringValue());
+				}
+				if(doc.getField(EdocDocument.FIELD_VERSIONNUM)!=null){
+					edoc.setVersionNum(doc.getField(EdocDocument.FIELD_VERSIONNUM).stringValue());
+				}
+				if(doc.getField(EdocDocument.FIELD_SOURCEFILENAME)!=null){
+					edoc.setSourceFileName(doc.getField(EdocDocument.FIELD_SOURCEFILENAME).stringValue());
+				}
+				
+				rs.add(edoc);
+			}
+		}
+		return rs;
+	}
+	
 
 	public int getRecordCount() {
 		return recordCount;
